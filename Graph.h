6625c6cc4be02d895a1ec6104e32f482631aee1c
Hyeop Lee <leehyeop18@gmail.com>
2020-04-08 16:26:22 -0400

    question regarding starting code

--- Graph.h
+++ Graph.h
@@ -8,50 +8,62 @@ using namespace std;
 typedef vector<vector<int> > Matrix;
 
 class Graph{
-
   Matrix graph;
 
-public:
-  Graph(int nNodes, int nEdges){
-    // you don't have to change anything here. This function will generate a random weighted graph given the
-    // desired parameters (number of nodes and number of edges)
-    srand (time(NULL));
+  public:
+    Graph(int nNodes, int nEdges){
+      // you don't have to change anything here. This function will generate a random weighted graph given the
+      // desired parameters (number of nodes and number of edges)
+      srand (time(NULL));
 
-    graph = Matrix(nNodes, vector<int>(nNodes,0));
+      graph = Matrix(nNodes, vector<int>(nNodes,0));
 
-    for(int i=0; i<nEdges; i++){
-      //random weight
-      int weight = rand()%(101);
-      int node1, node2;
+      for(int i=0; i<nEdges; i++){
+        //random weight
+        int weight = rand()%(101);
+        int node1, node2;
 
-      do{
-        node1 = rand()%(nNodes);
-        node2 = rand()%(nNodes);
-      }while(node1 == node2 && !connected(node1,node2));
+        do{
+          node1 = rand()%(nNodes);
+          node2 = rand()%(nNodes);
+          cout << "BEFORE ----> node1: " << node1 << "     node2: " << node2 << endl;
+        }while(node1 == node2 || !connected(node1,node2));
+        cout << "AFTER ----> node1: " << node1 << "     node2: " << node2 << endl<<endl;
+        graph[node1][node2] = graph[node2][node1] = weight;
+      }
 
-      graph[node1][node2] = graph[node2][node1] = weight;
     }
 
-  }
+    int size(){
+      //### return the number of nodes in the graph
+      return graph.size();
+    }
 
-  int size(){
-    //### return the number of nodes in the graph
-  }
+    list<int> getConnectedNodes(int n){
+      //### return a list with the nodes connected to node
+      list<int> connectedNodes;
+    
 
-  list<int> getConnectedNodes(int n){
-    //### return a list with the nodes connected to node
-  }
+      return connectedNodes;
+    }
 
-  int getEdgeWeight(int n, int n2){
-    //### return the weight assigned to a node
-  }
+    int getEdgeWeight(int n, int n2){
+      //### return the weight assigned to a node
+      return graph[n][n2];
+    }
 
-  bool connected(int n, int n2){
-    //### return true if the two nodes are connected
-  }
+    bool connected(int n, int n2){
+      //### return true if the two nodes are connected
+      return graph[n][n2] == graph[n2][n];
+    }
 
-  void print(){
-    //### you can implement this for debugging purposes
-  }
-
+    void print(){
+      //### you can implement this for debugging purposes
+      for( int i = 0; i < size(); i++ ) {
+        for( int j = 0; j < size(); j++ ) {
+          cout << "\t" << graph[i][j];
+        }
+        cout << endl;
+      }
+    } 
 };
--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 
 int MAX = numeric_limits<int>::max();
 
-vector<int> computeShortestPath(Graph g, int node){
+// vector<int> computeShortestPath(Graph g, int node){
 
-  //####implement the Dijkstra’s Algorithm here
+//   //####implement the Dijkstra’s Algorithm here
 
-};
+// };
 
-vector<vector<int> > computeAllPairsShortestPath(Graph g){
+// vector<vector<int> > computeAllPairsShortestPath(Graph g){
 
-  //####compute all possible shortest paths by calling the Dijkstra's Algorithm on each pair of nodes
+//   //####compute all possible shortest paths by calling the Dijkstra's Algorithm on each pair of nodes
 
-};
+// };
 
-Matrix computeAllPairsShortestPathDynamicProgramming(Graph g){
-  //BONUS
-  //####compute all possible shortest paths by implementing the dynamic programming approach
-};
+// Matrix computeAllPairsShortestPathDynamicProgramming(Graph g){
+//   //BONUS
+//   //####compute all possible shortest paths by implementing the dynamic programming approach
+// };
 
 bool compareResults(Graph g, Matrix a, Matrix b){
 
@@ -41,28 +41,29 @@ bool compareResults(Graph g, Matrix a, Matrix b){
 int main(){
 
   //create graph as a random matrix
-  Graph g(10,10);
+  Graph g(5,5);
   cout << "Graph generated " << endl;
+  g.print();
 
-  //compute shortest path
-  Timer time;
-  time.start();
-  Matrix a = computeAllPairsShortestPath(g);
-  time.stop();
-  cout << "Time with Dijkstra's algorithm" << time.getElapsedTimeInSec() << endl;
+  // //compute shortest path
+  // Timer time;
+  // time.start();
+  // Matrix a = computeAllPairsShortestPath(g);
+  // time.stop();
+  // cout << "Time with Dijkstra's algorithm" << time.getElapsedTimeInSec() << endl;
 
-  //compute all possible shortest paths
-  time.start();
-  Matrix b = computeAllPairsShortestPathDynamicProgramming(g);
-  time.stop();
-  cout << "Time with Dynamic programming" << time.getElapsedTimeInSec() << endl;
+  // //compute all possible shortest paths
+  // time.start();
+  // Matrix b = computeAllPairsShortestPathDynamicProgramming(g);
+  // time.stop();
+  // cout << "Time with Dynamic programming" << time.getElapsedTimeInSec() << endl;
 
-  //compute by dynamic programming approach
-  if(compareResults(g,a,b)){
-    cout << "They are the same" << endl;
-  }
-  else{
-    cout << "Different" << endl;
-  }
+  // //compute by dynamic programming approach
+  // if(compareResults(g,a,b)){
+  //   cout << "They are the same" << endl;
+  // }
+  // else{
+  //   cout << "Different" << endl;
+  // }
 
 };
